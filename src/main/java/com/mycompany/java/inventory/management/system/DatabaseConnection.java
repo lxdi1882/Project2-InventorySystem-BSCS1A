4cd813f958225460326af0a7ac0bacfee84e681f
lxdi1882 <lxdi1883@gmail.com>
2026-04-17 21:10:40 +0800

    Organized Source Files
    Organized Source Files for better communication and work flow

--- src/main/java/com/mycompany/java/inventory/management/system/DatabaseConnection.java
+++ src/main/java/com/mycompany/java/inventory/management/system/DatabaseConnection.java
@@ -1,18 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.java.inventory.management.system;
 
 /**
  *
  * @author lxdi1
  */
-public class JavaInventoryManagementSystem {
-
-    public static void main(String[] args) {
-        System.out.println("helo fren");
-        
-        System.out.println("wasp bbg");
-    }
+public class DatabaseConnection {
+    
 }
---
+++ src/main/java/com/mycompany/java/inventory/management/system/Project2InventorySystemBSCS1A.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Project2InventorySystemBSCS1A;
+
+/**
+ *
+ * @author lxdi1
+ */
+public class Project2InventorySystemBSCS1A {
+
+    public static void main(String[] args) {
+        System.out.println("helo fren");
+        
+        System.out.println("wasp bbg");
+    }
+}
